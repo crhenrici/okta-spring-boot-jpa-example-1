30c864d1fe2fc083a0e7de4c142a93c657babb33
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-11-21 20:56:04 -0800

    auth working

--- src/main/java/com/okta/springbootjpa/KayakRepository.java
+++ src/main/java/com/okta/springbootjpa/KayakRepository.java
@@ -2,11 +2,12 @@ package com.okta.springbootjpa;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 @RepositoryRestResource
-public interface KayakRepository extends CrudRepository<Kayak, Integer> {
-
+@PreAuthorize("hasAuthority('Admin')")
+public interface KayakRepository extends CrudRepository<Kayak, Long> {
 }
--- src/main/java/com/okta/springbootjpa/SpringBootJpaApplication.java
+++ src/main/java/com/okta/springbootjpa/SpringBootJpaApplication.java
@@ -4,18 +4,22 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.stream.Stream;
 
 @SpringBootApplication
+@EnableResourceServer
 public class SpringBootJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaApplication.class, args);
 	}
 
+	/*
 	@Bean
 	ApplicationRunner init(KayakRepository repository) {
 
@@ -43,5 +47,6 @@ public class SpringBootJpaApplication {
 			repository.findAll().forEach(System.out::println);
 		};
 	}
+	*/
 	
 }
