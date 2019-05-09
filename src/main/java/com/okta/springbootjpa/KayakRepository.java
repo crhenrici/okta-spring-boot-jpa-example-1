c31ffa3e991f5e61c7d5ba878567403454f6d363
henricri <henricri@students.zhaw.ch>
2019-05-09 15:57:27 +0200

    changes to KayakRepository

--- src/main/java/com/okta/springbootjpa/KayakRepository.java
+++ src/main/java/com/okta/springbootjpa/KayakRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource
-@PreAuthorize("hasAuthority('Admin')")
 public interface KayakRepository extends CrudRepository<Kayak, Integer> {
-
+    @PreAuthorize("hasAuthority('Admin')")
+    <S extends Kayak> S save(S entity);
 }
